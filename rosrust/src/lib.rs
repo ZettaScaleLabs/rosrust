4a094a3af5e46851dcaa353f07fdb13f16414493
yellowhatter <bannov.dy@gmail.com>
2023-09-28 15:05:56 +0400

    ignore Clippy for this inherited code

--- rosrust/src/lib.rs
+++ rosrust/src/lib.rs
@@ -1,28 +1,48 @@
 #![recursion_limit = "1024"]
-
+#[allow(clippy::all)]
 pub use crate::api::handlers::SubscriptionHandler;
+#[allow(clippy::all)]
 pub use crate::api::raii::{Publisher, Service, Subscriber};
+#[allow(clippy::all)]
 pub use crate::api::{error, Clock, Parameter};
+#[allow(clippy::all)]
 pub use crate::raw_message::{RawMessage, RawMessageDescription};
 #[doc(hidden)]
+#[allow(clippy::all)]
 pub use crate::rosmsg::RosMsg;
+#[allow(clippy::all)]
 pub use crate::singleton::*;
+#[allow(clippy::all)]
 pub use crate::tcpros::{Client, ClientResponse, Message, ServicePair, ServiceResult};
+#[allow(clippy::all)]
 pub use dynamic_msg::DynamicMsg;
+#[allow(clippy::all)]
 pub use ros_message::{Duration, MessageValue as MsgMessage, Time, Value as MsgValue};
 #[doc(hidden)]
+#[allow(clippy::all)]
 pub use rosrust_codegen::*;
+#[allow(clippy::all)]
 pub mod wall_time;
 
+#[allow(clippy::all)]
 pub mod api;
+#[allow(clippy::all)]
 mod dynamic_msg;
+#[allow(clippy::all)]
 mod log_macros;
 #[doc(hidden)]
+#[allow(clippy::all)]
 pub mod msg;
+#[allow(clippy::all)]
 mod raw_message;
 #[doc(hidden)]
+#[allow(clippy::all)]
 pub mod rosmsg;
+#[allow(clippy::all)]
 pub mod rosxmlrpc;
+#[allow(clippy::all)]
 pub mod singleton;
+#[allow(clippy::all)]
 pub mod tcpros;
+#[allow(clippy::all)]
 pub mod util;
